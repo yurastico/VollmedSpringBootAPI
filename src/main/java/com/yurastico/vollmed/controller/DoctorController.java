97781f6742b4b4e805c8f0b11e44c1ae85435204
yurastico <cunha.ysc@gmail.com>
2024-03-18 06:35:53 -0300

    sorting default response and showing sql in terminal

--- src/main/java/com/yurastico/vollmed/controller/DoctorController.java
+++ src/main/java/com/yurastico/vollmed/controller/DoctorController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,7 +27,7 @@ public class DoctorController {
     }
 
     @GetMapping
-    public Page<DoctorListingData> listDoctors(Pageable pagination) {
+    public Page<DoctorListingData> listDoctors(@PageableDefault(size = 10, sort = {"name"}) Pageable pagination) {
         return repository.findAll(pagination).map(DoctorListingData::new);
     }
 
