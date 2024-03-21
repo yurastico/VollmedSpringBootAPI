eafb27923f5b04edc2ffbf2ceff639c98dfd1162
yurastico <cunha.ysc@gmail.com>
2024-03-21 13:31:53 -0300

    creating User entity

---
+++ src/main/java/com/yurastico/vollmed/domain/user/User.java
@@ -0,0 +1,21 @@
+package com.yurastico.vollmed.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+}
