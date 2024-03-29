daa82f33101575f460773fa552f7f1fed5c4de44
yurastico <cunha.ysc@gmail.com>
2024-03-29 13:10:22 -0300

    creating Patient entity

--- src/main/java/com/yurastico/vollmed/domain/patient/Patient.java
+++ src/main/java/com/yurastico/vollmed/domain/patient/Patient.java
@@ -1,4 +1,25 @@
 package com.yurastico.vollmed.domain.patient;
 
+import com.yurastico.vollmed.domain.address.Address;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Table(name = "patients")
+@Entity(name = "Patient")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Patient {
+    private String name;
+    private String email;
+    private String phone;
+    private String cpf;
+    private Address address;
+
 }
