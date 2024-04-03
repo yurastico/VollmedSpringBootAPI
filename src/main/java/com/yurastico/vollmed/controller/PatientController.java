49e1ca6503116d2d21fb6466775e33e79147c288
yurastico <cunha.ysc@gmail.com>
2024-04-02 22:08:56 -0300

    creating patients

--- src/main/java/com/yurastico/vollmed/controller/PatientController.java
+++ src/main/java/com/yurastico/vollmed/controller/PatientController.java
@@ -1,11 +1,29 @@
 package com.yurastico.vollmed.controller;
 
+import com.yurastico.vollmed.domain.doctor.DetailDoctorData;
+import com.yurastico.vollmed.domain.doctor.Doctor;
+import com.yurastico.vollmed.domain.doctor.DoctorCreateData;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/patients")
 public class PatientController {
+    @PostMapping
+    @Transactional
+    public ResponseEntity createPatient(@RequestBody @Valid PatientCreateData data, UriComponentsBuilder uriBuilder) {
+        var doctor = new Patient(data);
+        repository.save(doctor);
+        var uri = uriBuilder.path("/patients/{id}").buildAndExpand(doctor.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DetailDoctorData(patient));
+
+    }
 
 
 
